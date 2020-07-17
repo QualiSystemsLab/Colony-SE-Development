a4fbf5e744e63787150ac49e589761774346db37
Omri Avraham <omritk@gmail.com>
2020-07-17 16:10:08 -0600

    ..

--- applications/StormRunnerAgent/StormRunnerAgent.ps1
+++ applications/StormRunnerAgent/StormRunnerAgent.ps1
@@ -1,1 +1,10 @@
-'This is some text' | Out-File -FilePath 'C:/aa.txt'
+Start-Transcript -path C:\output.txt -append
+dir env:
+
+# Choco & friends
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install googlechrome -y
+choco install firefox -y
+# Microsoft Visual C++ Redistributable for Visual Studio 2015 Update 3 (with hotfix 2016-09-14)
+choco install vcredist140 --version=14.0.24215.1 -y
